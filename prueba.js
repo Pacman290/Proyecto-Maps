cd5a8b3c230358d1a397ac4f620e0464a6adf07a
Paco Lovera <pacolovera29@gmail.com>
2024-01-24 14:36:51 -0300

    agregando comentarios y mejorando el código js

--- prueba.js
+++ prueba.js
@@ -3,7 +3,8 @@ let autocompleteDestination;
 let service;
 
 function iniciar(){
-  document.getElementById("enviarLocalidades").addEventListener("click", mostrarDistanciaTiempo, false);
+  document.getElementById("enviarLocalidades").addEventListener("click", consultarDistanciaTiempo, false);
+  // Inicializa los autocompletados de Google Maps
   autocompleteOrigin = new google.maps.places.Autocomplete(
   document.getElementById("origenInput"),
   {
@@ -18,12 +19,17 @@ function iniciar(){
 }
 
 
-function mostrarDistanciaTiempo(){
-    const options ={
-      departureTime: new Date,
-      //trafficModel: 'optimistic'
-      //trafficModel: 'pessimistic'
-    }
+function consultarDistanciaTiempo(){
+  const options ={
+    departureTime: new Date,
+    //trafficModel: 'optimistic'
+    //trafficModel: 'pessimistic'
+  }
+  // chequea que los campos estén completos
+  if (!autocompleteOrigin.getPlace() || !autocompleteDestination.getPlace()) {
+    alert('Falta ingresar origen o destino');
+    return;
+  }
   const origen = autocompleteOrigin.getPlace().geometry.location;
   const destino = autocompleteDestination.getPlace().geometry.location;
   service.getDistanceMatrix(
@@ -32,24 +38,21 @@ function mostrarDistanciaTiempo(){
       destinations: [destino],
       travelMode: 'DRIVING',
       drivingOptions: options,
-    }, callback);
+    }, mostrarResultado);
 }
 
-function callback(response, status) {
+function mostrarResultado(response, status) {
   if (status == 'OK') {
+    //procesa y muestra los resultados
     const origins = response.originAddresses;
     const destinations = response.destinationAddresses;
-    let results;
-    let element;
-    for (let i = 0; i < origins.length; i++) {
-      results = response.rows[i].elements;
-      for (let j = 0; j < results.length; j++) {
-        element = results[j];
-        document.getElementById("distancia").innerHTML= element.distance.text;
-        document.getElementById("tiempo").innerHTML=element.duration_in_traffic.text;
-        document.getElementById("origen").innerHTML= origins[i];
-        document.getElementById("destino").innerHTML= destinations[j];
-      }
-    }
+    const results = response.rows[0].elements;
+    const element = results[0];
+    document.getElementById("distancia").innerHTML= element.distance.text;
+    document.getElementById("tiempo").innerHTML=element.duration_in_traffic.text;
+    document.getElementById("origen").innerHTML= origins[0];
+    document.getElementById("destino").innerHTML= destinations[0];
+  } else{
+    console.log('hubo un problema en la consulta');
   }
 }
